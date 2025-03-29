8b7a2eeb5aa39def1b8f0a85963f14b94fafefcc
Akolade Oladele <aoladele52@gmail.com>
2025-03-29 21:22:05 +0100

    task 2 completed

--- Task 2.py
+++ Task 2.py
@@ -1,5 +1,28 @@
-try:
-    x = float(input("firtst number: "))
-    y = float(input("second number: "))
-except ValueError:
-    print("Please enter a valid number")
+try:   #Exception handling for invalid or wrong input
+    x = float(input("firtst number: "))  #first number
+    y = float(input("second number: "))  #second number
+except ValueError:     #catch wrong inputerror
+    print("Please enter a valid number")  #error response
+operation = input("Enter operation (+, -, *, /, **, //): ")  #operation to perform
+if operation not in ['+', '-', '*', '/', '**', '//']:   #check if operation is valid
+    print("Invalid operation")
+else:
+    if operation == '+':   #addition
+        result = x + y
+    elif operation == '-':  #subtraction
+        result = x - y
+    elif operation == '*':    #multiplication
+        result = x * y
+    elif operation == '/':    #division
+        if y != 0:  #check if second number is not zero
+            result = x / y
+        else:
+            result = "Cannot divide by zero"   #detect division by zero
+    elif operation == '**':    #exponential or raised to the power
+        result = x ** y
+    elif operation == '//':     #Xth root of Y
+        if y != 0:    #check if second number is not zero
+            result = x // y
+        else:
+            result = "Cannot divide by zero"    #detect division by zero
+print(result)   #Display result
